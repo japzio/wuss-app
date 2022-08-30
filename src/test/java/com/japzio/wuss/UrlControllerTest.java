33b7d862a6b14416f98632a6a79ba2c552dfe60e
Jasper Culong <jculongit10@gmail.com>
2022-08-30 20:52:02 +0800

    urlcontroller test in progress, not yet working; deps upgrade

--- src/test/java/com/japzio/wuss/UrlControllerTest.java
+++ src/test/java/com/japzio/wuss/UrlControllerTest.java
@@ -1,12 +1,45 @@
 package com.japzio.wuss;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@ExtendWith(SpringExtension.class)
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-public class UrlControllerTest {
+@Testcontainers
+class UrlControllerTest {
+
+    @Container
+    static GenericContainer<?> redis =
+                new GenericContainer<>(DockerImageName.parse("redis:5.0.3-alpine"))
+                        .withExposedPorts(6379)
+                        .waitingFor(Wait.forHealthcheck());
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", redis::getHost);
+        registry.add("spring.redis.port", redis::getFirstMappedPort);
+    }
+
+    @BeforeAll
+    public static void setup(){
+        redis.start();
+    }
+
+    @Test
+    public void defaultTest() {
+        assertTrue(true);
+    }
+
 
 }
 
